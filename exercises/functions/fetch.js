1cee9f6c10c1fa926bd9cfe71600de4c7acc0822
BGDSHKA <kirill.ateev59@gmail.com>
2021-02-26 22:36:53 +0300

    fetch task

---
+++ exercises/functions/fetch.js
@@ -0,0 +1,5 @@
+export async function task(x) {
+    const resp = await fetch(`https://kodaktor.ru/api/m/${x}`, {headers: {'Content-Type': 'application/json'}});
+    const result = await resp.json()
+    return result.result
+}
