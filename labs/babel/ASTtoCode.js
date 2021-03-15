a3ce3d63d2494736353e7825307ef24f005133ac
BGDSHKA <kirill.ateev59@gmail.com>
2021-03-15 20:19:28 +0300

    babel lab 1

---
+++ labs/babel/ASTtoCode.js
@@ -0,0 +1,12 @@
+const {default: generate} = require('@babel/generator');
+const {get} = require('http');
+
+const URL = 'http://kodaktor.ru/j/_ast/23ab6';
+
+get(URL, (r, ast = '') => r
+.on('data', d=>ast+=d)
+.on('end', () => {
+    const {code} = generate(JSON.parse(ast));
+    console.log(code);
+})
+);
