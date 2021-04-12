cd321da7a61e3bdf24c4a416cc61e47835f3e667
BGDSHKA <kirill.ateev59@gmail.com>
2021-04-12 20:58:35 +0300

    iterations

--- labs/Generators/Lab_generators.js
+++ labs/Generators/Lab_generators.js
@@ -1,19 +1,19 @@
 function* iterable2(...args) {
-    yield 'hello';
-    yield 'world';
-    }
-    
-    const myiter2 = iterable2()
-    
-    console.log(myiter2.next().value)
-    console.log(myiter2.next().value)
-    console.log(myiter2.next().value)
-    console.log('************')
-    
-   const myiter3 = iterable2(1,2,3,4)
-   for (let val of myiter3) {
-     console.log(val);
-   }
-    
-    const myiter4 = iterable2(1,2,3,4)
-    console.log(...myiter4)
+  yield "hello";
+  yield "world";
+}
+
+const myiter2 = iterable2();
+
+console.log(myiter2.next().value);
+console.log(myiter2.next().value);
+console.log(myiter2.next().value);
+console.log("************");
+
+const myiter3 = iterable2(1, 2, 3, 4);
+for (let val of myiter3) {
+  console.log(val);
+}
+
+const myiter4 = iterable2(1, 2, 3, 4);
+console.log(...myiter4);
--- labs/babel/ASTtoCode.js
+++ labs/babel/ASTtoCode.js
@@ -1,12 +1,13 @@
-const {default: generate} = require('@babel/generator');
-const {get} = require('http');
+const { default: generate } = require("@babel/generator");
+const { get } = require("http");
 
-const URL = 'http://kodaktor.ru/j/_ast/23ab6';
+const URL = "http://kodaktor.ru/j/_ast/23ab6";
 
-get(URL, (r, ast = '') => r
-.on('data', d=>ast+=d)
-.on('end', () => {
-    const {code} = generate(JSON.parse(ast));
-    console.log(code);
-})
+get(URL, (r, ast = "") =>
+  r
+    .on("data", (d) => (ast += d))
+    .on("end", () => {
+      const { code } = generate(JSON.parse(ast));
+      console.log(code);
+    })
 );
