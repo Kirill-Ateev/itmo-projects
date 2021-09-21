810f4294d9d224c51a704057496fa60d50c67fa0
BGDSHKA <kirill.ateev59@gmail.com>
2021-09-21 22:08:51 +0300

    chore: week 4 task correct

--- exercises/express/server.js
+++ exercises/express/server.js
@@ -4,13 +4,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 const CORS = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,POST,DELETE,PUT,OPTIONS,PATCH",
+  "Access-Control-Allow-Methods": "GET,POST,DELETE,PUT,OPTIONS",
   "Access-Control-Allow-Headers":
-    "x-test,Content-Type,Accept,Access-Control-Allow-Headers",
-  "Access-Control-Expose-Headers":
-    "X-Resp,Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Expose-Headers",
-  "Access-Control-Allow-Headers":
-    "X-Resp,Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Expose-Headers",
+    "x-test,Content-Type,Accept,Access-Control-Allow-Headers"
 };
 const login = 'itmo307692';
 const headersTextHtml = {
