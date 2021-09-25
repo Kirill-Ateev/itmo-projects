90581a1c6aea8a12dab65908337ef57b6d75188e
BGDSHKA <kirill.ateev59@gmail.com>
2021-09-25 14:22:31 +0300

    chore: net task

--- labs/sockets/net.js
+++ labs/sockets/net.js
@@ -1,1 +1,2 @@
+//not for apache servers (\n should be replaced by \r\n\r?)
 void net.connect(80, '77.234.202.142').on('data', x => console.log(String(x))).write('POST /g/post.php HTTP/1.1\nHost:node-server.online\nContent-Type:application/x-www-form-urlencoded\nContent-Length:9\n\nname=Kirill');
