558d555d1bf68097a8bdedd3cf669034fdf8e879
BGDSHKA <kirill.ateev59@gmail.com>
2021-09-26 15:20:11 +0300

    chore: new express task

--- exercises/express/server.js
+++ exercises/express/server.js
@@ -3,9 +3,10 @@ const express = require('express');
 const PORT = process.env.PORT || 5000;
 const app = express();
 const CORS = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-  "Access-Control-Allow-Headers": "x-test,Content-Type,Accept,Access-Control-Allow-Headers"
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+  'Access-Control-Allow-Headers':
+    'x-test,Content-Type,Accept,Access-Control-Allow-Headers',
 };
 const login = 'itmo307692';
 const headersTextHtml = {
@@ -22,12 +23,11 @@ const headersJson = {
   'Content-Type': 'application/json',
 };
 
-
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.set(headersTextHtml);
-  console.log(req)
+  console.log(req);
   res.send(login);
 });
 
@@ -35,7 +35,7 @@ app.get('/f', (r) => {
   res.format({
     'text/html': () => r.res.send('<h2>42<h2>'),
     'application/json': () => r.res.send('<h2>Ht42<h2>'),
-  })
+  });
   res.send(login);
 });
 
@@ -77,21 +77,21 @@ app.get('/fetch/', (req, res) => {
     </html>`);
 });
 
-app.get("/result4/", (req, res) => {
+app.get('/result4/', (req, res) => {
   const result = {
     message: login,
-    "x-result": req.headers["x-test"],
+    'x-result': req.headers['x-test'],
   };
-  let body = "";
+  let body = '';
 
   req
-  .on("data", (chunk) => (body += chunk))
-  .on("end", () => {
-    result["x-body"] = body;
-    res.writeHead(200, {headersJson, ...CORS});
-    res.end(JSON.stringify(result));
-  });
-})
+    .on('data', (chunk) => (body += chunk))
+    .on('end', () => {
+      result['x-body'] = body;
+      res.writeHead(200, { headersJson, ...CORS });
+      res.end(JSON.stringify(result));
+    });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
