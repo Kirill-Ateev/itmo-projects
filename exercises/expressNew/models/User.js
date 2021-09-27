6fbaf340ba7f5ab4b6a072cc4daa80ce06c79556
BGDSHKA <kirill.ateev59@gmail.com>
2021-09-27 22:19:23 +0300

    chore: mongo task

---
+++ exercises/expressNew/models/User.js
@@ -0,0 +1,7 @@
+export default (mongoose) => {
+  const UserSchema = mongoose.Schema({
+    login: String,
+    password: { type: String, required: [true, 'Password is a necessity'] },
+  });
+  return mongoose.model('User', UserSchema);
+};
---
+++ exercises/expressNew/routes/UserController.js
@@ -0,0 +1,33 @@
+import headersTextPlain from '../app.js';
+
+export default (express, User) => {
+  const router = express.Router();
+
+  router
+    .route('/')
+    .get(async (r) => r.res.json(await User.find()))
+    .post(async (req, res) => {
+      res.set(headersTextPlain);
+
+      const { login, password, URL } = JSON.parse(req.body);
+      const newUser = new User({ login, password });
+
+      try {
+        await mongoose.connect(URL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        await newUser.save();
+        res.status(201).json({ 'Добавлено: ': login });
+      } catch (e) {
+        res.status(400).json({ Ошибка: `${e}` });
+      }
+    });
+
+  router.route('/:login').get(async (r) => {
+    const { login } = r.params;
+    r.res.json(await User.find({ login }));
+  });
+
+  return router;
+};
