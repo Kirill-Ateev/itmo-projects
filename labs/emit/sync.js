982ddf18a0cfcc2881256e963ba3c76b4baf113d
BGDSHKA <kirill.ateev59@gmail.com>
2021-10-02 13:45:33 +0300

    emit

--- labs/emit/sync.js
+++ labs/emit/sync.js
@@ -7,4 +7,6 @@ console.log(1)
 e.emit('hello')
 console.log(2)
 
-// 1 2.5 2
+// 1 2.5 2 Происходит синхронно! Чтобы было асинхронно 
+//можно обернуть console.log в setImmediate или setTimeout чтобы 
+//вызов попал в очередь
